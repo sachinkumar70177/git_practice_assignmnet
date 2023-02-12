9c5c1826e79ce5b11a31b3aba242f9dbba95a396
unknown <sachinpatel.k.786@gami.com>
2023-02-12 16:29:42 +0530

    i have written a prime number code is it correct

---
+++ prime.js
@@ -0,0 +1,14 @@
+// code for prime number.
+let x=5;
+let count=0;
+for(i=1;i<=5;i++){
+if(5%i==0){
+count++;
+}
+}
+if (count==2){
+console.log(x+"is a prime number");
+}
+else {
+console.log(x+"is not a prime number")
+}
